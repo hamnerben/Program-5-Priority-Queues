1a93b6932b92672584868c1087cb3c911b5a2c96
hamnerben <hamnerben@gmail.com>
2022-10-25 18:27:23 -0600

    gitignore updated

--- src/Task1.java
+++ src/Task1.java
@@ -3,7 +3,7 @@ public class Task1 extends Task {
     public Task1(int ID, int start, int deadline, int duration) {
         super(ID,start,deadline,duration);
     }
-    // Prioirity is deadline
+    // Priority is deadline
     @Override
     public int compareTo(Task t2) {
          //System.out.println("Using Task1 compareTo");
